697cb62c07614987bbde0cec7f57bd900892587e
kalynb700 <131439993+kalynb700@users.noreply.github.com>
2025-03-20 00:56:42 -0700

    abstract class initial commit

--- animals/Cat.java
+++ animals/Cat.java
@@ -1,3 +1,28 @@
 public class Cat extends Pet {
+
+    String name;
+    String breed;
+    int age;
+    int roomNumber;
+    Boolean isHealthy;
+    Boolean isAdopted;
+    Boolean isLitterBoxTrained;
+
     
+
+    public void addNewPet(){}
+    public void updatePetProfile(){}
+    public String getPetProfile(){ 
+        String egu = "";
+        return egu;
+}
+    public void setAdoptionStatus(){}
+    public void adopt(String guestname, String petName){}
+    
+    public void play(String guestName){}
+    public void setAppointment(String guestName){}
+    public void cancelAppointment(String guestName){}
+    public void checkMeetingTime(){}
+    public void eat(){}
+    public void vetTrip(Vet vet){}
 }
--- animals/Hamster.java
+++ animals/Hamster.java
@@ -1,3 +1,43 @@
 public class Hamster extends Pet {
+
+    String name;
+    String breed;
+    int age;
+    int roomNumber;
+    Boolean isHealthy;
+    Boolean isAdopted;
+    Boolean usesWheel;
+
+
+    public void setAdoptionStatus(){}
+
+    public void adopt(String guestname, String petName){}
+
+    public void play(String guestName){}
+
+    public void setAppointment(String guestName){}
+
+    public void cancelAppointment(String guestName){}
+
+    public void checkMeetingTime(){}
+
+    public void eat(){}
+
+    public boolean vetTrip(Vet vet){
+        return false;
+    }
+
+    @Override 
+    public void addNewPet(){}
+
+    @Override 
+    public void updatePetProfile(){}
+
+    @Override 
+    public String getPetProfile(){ 
+        String egu = "";
+        return egu;
+    }
+
     
 }
